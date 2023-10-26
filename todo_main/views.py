a1d2cec6199cd5dbef1d59dd96696b5166937594
Emil Roydev <e.roydev@gmail.com>
2023-10-26 02:15:23 -0700

    final commit

--- todo_main/views.py
+++ todo_main/views.py
@@ -7,8 +7,10 @@ def home(request):
   
   tasks_objects = Task.objects.filter(is_completed=False).order_by('updated_at')
 
+  completed_tasks = Task.objects.filter(is_completed=True)
   context = {
-    'tasks': tasks_objects
+    'tasks': tasks_objects,
+    'completed': completed_tasks
   }
 
   return render(request, 'home.html', context)
